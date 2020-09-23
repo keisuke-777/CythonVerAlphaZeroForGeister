04ee8b5963eddd5476ee6a4909b6229e035d8871
keisuke <keisuke7771120@me.com>
2020-09-23 16:00:25 +0900

    初期コミット

---
+++ PseudoCode.py
@@ -0,0 +1,62 @@
+# 疑似コード
+
+# パターンリスト
+EnPatternList = []
+MyPatternList = []
+
+# パターン格納
+EnPattern = int[70]
+MyPattern = int[70]
+
+gamma = 0.9
+
+
+def fight():
+
+    # 相手の行動を受け取る(相手の行動は自分の駒配置をある程度予測するはず)
+
+    # 相手の盤面を推定する
+    suiteiValue = banmenSuitei()
+
+    # 一番価値の高い行動を選択
+
+    pass
+
+
+# 相手の盤面を推定する(引数：盤面+行動、ニューラルネット、過去の盤面推定値)
+def banmenSuitei():
+    for EnPatternList in e:
+        EnNuraruValue = 0
+        for MyPatternList in m:
+            # ニューラルネットを使って価値を算出
+            EnNuraruValue += nuraruValue
+            # MyPattern[i] += MyPattern[i] * gamma + nuraruValue
+        # 相手の盤面推定値を更新
+        EnPattern[i] += EnPattern[i] * gamma + EnNuraruValue
+
+    pass
+    return null
+
+
+# 盤面推定値を使って行動決定
+def KoudouKettei():
+    EnPattern  # 70の値
+    RealMyPattern  # 1通り
+    moveMargeList
+    for EnPattern in i:
+        # ニューラルネットを使って価値を算出
+        moveValueList = nuraruValue(EnPattern[i], RealMyPattern)
+        # 書き方間違ってそうだけど、合併(全要素を足し合わせる)
+        moveMargeList += moveValueList
+
+    pass
+
+
+# ニューラルネットで価値のリストを返す
+def nuraruValue():
+    pass
+
+
+# 動作確認
+if __name__ == "__main__":
+    fight()
