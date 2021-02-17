9f69fa0e8e97d163e522e34d93292475957de47c
keisuke <keisuke7771120@me.com>
2021-02-17 20:11:42 +0900

    win環境で動くようにコメントを追記

--- setup.py
+++ setup.py
@@ -4,3 +4,10 @@ import numpy
 
 setup(ext_modules=cythonize("game.pyx"), include_dirs=[numpy.get_include()])
 
+
+# cythonize -a -i game.pyx
+
+
+# cythonize -i game.pyx
+# cythonize -i pv_mcts.pyx
+# cythonize -i self_play.pyx
