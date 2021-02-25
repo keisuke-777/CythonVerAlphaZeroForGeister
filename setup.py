223c9deb6b1d73ead5a05408528eda773524800c
keisuke <keisuke7771120@me.com>
2021-02-25 18:08:50 +0900

    joblibで並列処理

--- setup.py
+++ setup.py
@@ -6,7 +6,19 @@ setup(ext_modules=cythonize("game.pyx"), include_dirs=[numpy.get_include()])
 
 setup(ext_modules=cythonize("pv_mcts.pyx"), include_dirs=[numpy.get_include()])
 
-setup(name="self_play", ext_modules=[Extension("self_play", ["self_play.c"])])
+setup(ext_modules=cythonize("self_play.pyx"), include_dirs=[numpy.get_include()])
+
+
+# extensions = [
+#     Extension(
+#         "self_play",
+#         ["self_play.pyx"],
+#         extra_compile_args=["-fopenmp"],
+#         extra_link_args=["-fopenmp"],
+#     )
+# ]
+# setup(ext_modules=cythonize(extensions))
+
 
 # cythonize -a -i game.pyx
 
